eaca8b9422704b4a61196c85d64b9ad723f55e2f
Isaac Smith <103906316+ismith916@users.noreply.github.com>
2023-05-12 17:06:08 -0700

    react front end base homepage and navbar work

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div><p>&copy; 2023 All Rights Reserved - Bethel Tech - MannaSoft</p></div>
+    )
+}
+
+export default Footer
---
+++ src/components/ViewAllFiles.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ViewAllFiles = () => {
+    return (
+        <div>ViewAllFiles</div>
+    )
+}
+
+export default ViewAllFiles
